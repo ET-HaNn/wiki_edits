8699a7caa97afe1bf63dee5142bb1c77722d1075
ethan <ecrump@bsu.edu>
2022-09-18 19:28:47 -0400

    Start of the project. Made 'network_finder' able to get link for any database I.e. Input 'Soup', and get the editor detail link.

---
+++ lib/network_finder.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+
+class NetworkReader {
+  Future<String> read(Uri uri) async {
+    String editLink = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Joe_Biden&rvprop=timestamp|user&rvlimit=4&redirects";
+    String result = editLink.replaceAll("Soup", "_controller");
+    final response = await http.get(Uri.parse(result));
+    return response.body;
+  }
+}
